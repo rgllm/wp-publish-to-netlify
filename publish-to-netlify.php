c1df996340b1d40114f4f946d62c79d604aa21dc
Rogério Moreira <r@rgllm.com>
2019-08-14 11:15:59 +0100

    update plugin version

--- publish-to-netlify.php
+++ publish-to-netlify.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://rgllm.com
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Publish_To_Netlify
  *
  * @wordpress-plugin
